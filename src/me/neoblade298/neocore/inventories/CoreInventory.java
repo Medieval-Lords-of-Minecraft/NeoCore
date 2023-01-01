484d82fe8484e7ded49e79be1c1afe2cdd1d7094
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-12-31 22:03:55 -0800

    Change core inventory visibility

--- src/me/neoblade298/neocore/inventories/CoreInventory.java
+++ src/me/neoblade298/neocore/inventories/CoreInventory.java
@@ -7,8 +7,8 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 
 public abstract class CoreInventory {
-	private Inventory inv;
-	private Player p;
+	protected Inventory inv;
+	protected Player p;
 	public abstract void handleInventoryClick(InventoryClickEvent e);
 	public abstract void handleInventoryDrag(InventoryDragEvent e);
 	public abstract void handleInventoryClose(InventoryCloseEvent e);
