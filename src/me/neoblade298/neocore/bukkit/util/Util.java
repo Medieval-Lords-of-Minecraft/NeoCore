5e1bb6cae7d93ca98212f6551f64701223e67ce9
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2023-02-09 16:54:33 -0800

    Add base 64 itemstack converter

--- src/me/neoblade298/neocore/bukkit/util/Util.java
+++ src/me/neoblade298/neocore/bukkit/util/Util.java
@@ -1,5 +1,8 @@
 package me.neoblade298.neocore.bukkit.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Collection;
 
 import org.bukkit.Bukkit;
@@ -7,6 +10,10 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 import me.neoblade298.neocore.shared.util.SharedUtil;
 
@@ -66,4 +73,45 @@ public class Util {
 		}
 		return str;
 	}
+	
+	
+    public static String toBase64(ItemStack[] items) throws IllegalStateException {
+    	try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+            
+            // Write the size of the array
+            dataOutput.writeInt(items.length);
+            
+            // Save every element in the list
+            for (int i = 0; i < items.length; i++) {
+                dataOutput.writeObject(items[i]);
+            }
+            
+            // Serialize that array
+            dataOutput.close();
+            return Base64Coder.encodeLines(outputStream.toByteArray());
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
+        }
+    }
+
+    public static ItemStack[] fromBase64(String data) throws IOException {
+    	try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack[] items = new ItemStack[dataInput.readInt()];
+    
+            // Read the serialized array
+            for (int i = 0; i < items.length; i++) {
+            	items[i] = (ItemStack) dataInput.readObject();
+            }
+            
+            dataInput.close();
+            return items;
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Unable to decode class type.", e);
+        }
+    }
+	
 }
