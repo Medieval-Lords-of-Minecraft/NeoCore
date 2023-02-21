507f9daa5d6aea82b6d54d851290f9328ac3086b
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2023-02-21 14:15:34 -0800

    Fix broadcast not translating colors

--- src/me/neoblade298/neocore/bukkit/commands/builtin/CmdCoreBroadcast.java
+++ src/me/neoblade298/neocore/bukkit/commands/builtin/CmdCoreBroadcast.java
@@ -20,7 +20,7 @@ public class CmdCoreBroadcast extends Subcommand {
 			Util.msg(s, "&cYou need a message to broadcast!");
 		}
 		else {
-			Bukkit.broadcastMessage("&4[&c&lMLMC&4] " + SharedUtil.translateColors(SharedUtil.connectArgs(args)));
+			Bukkit.broadcastMessage(SharedUtil.translateColors("&4[&c&lMLMC&4] " + SharedUtil.connectArgs(args)));
 		}
 	}
 }
