c2f9c3fe2700839e258beaf4cb1252af53e43a4f
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2023-07-27 15:54:21 -0700

    Add drawEdges for rectangle

--- src/me/neoblade298/neocore/bukkit/particles/ParticleShape.java
+++ src/me/neoblade298/neocore/bukkit/particles/ParticleShape.java
@@ -1,12 +1,10 @@
 package me.neoblade298.neocore.bukkit.particles;
 
-import java.util.ArrayList;
-
-import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Particle.DustOptions;
 import org.bukkit.entity.Player;
 
 public abstract class ParticleShape {
 	public abstract void draw(Player p, boolean showAllPlayers, Particle part, DustOptions options);
+	public abstract void drawEdges(Player p, boolean showAllPlayers, Particle part, DustOptions options);
 }
--- src/me/neoblade298/neocore/bukkit/particles/Rectangle.java
+++ src/me/neoblade298/neocore/bukkit/particles/Rectangle.java
@@ -12,8 +12,8 @@ public class Rectangle extends ParticleShape {
 	
 	private Location bottomLeft;
 	private int heightIterations;
-	private double blocksPerParticle;
-	private Vector vWidth, vHeight;
+	private double blocksPerParticle, width, height;
+	private Vector vIterWidth, vIterHeight, vWidth, vHeight;
 	
 	public Rectangle(Player p, double width, double height, double distanceFromPlayer) {
 		this(p, width, height, distanceFromPlayer, 1 / PARTICLES_PER_METER);
@@ -23,25 +23,42 @@ public class Rectangle extends ParticleShape {
 	public Rectangle(Player p, double width, double height, double distanceFromPlayer, double blocksPerParticle) {
 		this.heightIterations = (int) (height / blocksPerParticle);
 		this.blocksPerParticle = blocksPerParticle;
+		this.width = width;
+		this.height = height;
 		
 		Vector localForward = p.getEyeLocation().getDirection().normalize();
 		Vector localLeft = localForward.clone().setY(0).rotateAroundY(Math.PI / 2);
 		Vector localUp = localForward.clone().rotateAroundAxis(localLeft, -Math.PI / 2);
+		
+		vWidth = localLeft.clone().multiply(-width);
+		vHeight = localUp.clone().multiply(height);
 
-		vWidth = localLeft.clone().multiply(width / 2);
-		vHeight = localUp.multiply(blocksPerParticle);
+		vIterWidth = localLeft.clone().multiply(width / 2);
+		vIterHeight = localUp.multiply(blocksPerParticle);
 		bottomLeft = p.getLocation().add(localForward.multiply(distanceFromPlayer));
-		bottomLeft.add(vWidth);
-		vWidth.multiply(-2);
+		bottomLeft.add(vIterWidth);
+		vIterWidth.multiply(-2);
 	}
 	
 	@Override
 	public void draw(Player p, boolean showAllPlayers, Particle part, DustOptions options) {
 		Location left = bottomLeft.clone();
 		for (int i = 0; i < heightIterations; i++) {
-			Location right = left.clone().add(vWidth);
+			Location right = left.clone().add(vIterWidth);
 			ParticleUtil.drawLine(p, left, right, part, showAllPlayers, 1, 0, 0, blocksPerParticle, options);
-			left.add(vHeight);
+			left.add(vIterHeight);
 		}
+	}
+	
+	@Override
+	public void drawEdges(Player p, boolean showAllPlayers, Particle part, DustOptions options) {
+		Location bl = bottomLeft.clone();
+		Location br = bl.clone().add(vWidth);
+		Location tl = bl.clone().add(vHeight);
+		Location tr = tl.clone().add(vWidth);
+		ParticleUtil.drawLine(p, bl, br, part, showAllPlayers, 1, 0, 0, blocksPerParticle, options);
+		ParticleUtil.drawLine(p, br, tr, part, showAllPlayers, 1, 0, 0, blocksPerParticle, options);
+		ParticleUtil.drawLine(p, tr, tl, part, showAllPlayers, 1, 0, 0, blocksPerParticle, options);
+		ParticleUtil.drawLine(p, tl, bl, part, showAllPlayers, 1, 0, 0, blocksPerParticle, options);
 	}
 }
