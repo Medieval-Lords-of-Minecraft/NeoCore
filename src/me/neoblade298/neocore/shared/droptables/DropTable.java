1d46212748d02cfe52c57224f70b014eda1518d3
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2023-08-13 16:41:11 -0700

    Rework droptable system

--- src/me/neoblade298/neocore/shared/droptables/DropTable.java
+++ src/me/neoblade298/neocore/shared/droptables/DropTable.java
@@ -4,26 +4,26 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Random;
 
-public class DropTable<E extends Droppable> {
-	private LinkedList<E> drops = new LinkedList<E>();
+public class DropTable<E> {
+	private LinkedList<Droppable<E>> drops = new LinkedList<Droppable<E>>();
 	private int totalWeight = 0;
 	private static Random gen = new Random();
 	
-	public void add(E drop) {
-		drops.add(drop);
-		totalWeight += drop.getWeight();
+	public void add(E drop, int weight) {
+		drops.add(new Droppable<E>(drop, weight));
+		totalWeight += weight;
 	}
 	
 	public E get() {
 		int rand = gen.nextInt(totalWeight);
 		
-		Iterator<E> iter = rand > totalWeight / 2 ? drops.descendingIterator() : drops.iterator();
-		E toReturn = null;
+		Iterator<Droppable<E>> iter = rand > totalWeight / 2 ? drops.descendingIterator() : drops.iterator();
+		Droppable<E> toReturn = null;
 		while (rand > 0) {
 			toReturn = iter.next();
 			rand -= toReturn.getWeight();
 		}
 		
-		return toReturn;
+		return toReturn.get();
 	}
 }
--- src/me/neoblade298/neocore/shared/droptables/Droppable.java
+++ src/me/neoblade298/neocore/shared/droptables/Droppable.java
@@ -1,13 +1,23 @@
 package me.neoblade298.neocore.shared.droptables;
 
-public class Droppable {
+public class Droppable<E> {
 	private int weight;
+	private E drop;
 	
-	public Droppable(int weight) {
+	public Droppable(E drop) {
+		this.drop = drop;
+	}
+	
+	public Droppable(E drop, int weight) {
+		this.drop = drop;
 		this.weight = weight;
 	}
 	
 	public int getWeight() {
 		return weight;
+	}
+	
+	public E get() {
+		return drop;
 	}
 }
