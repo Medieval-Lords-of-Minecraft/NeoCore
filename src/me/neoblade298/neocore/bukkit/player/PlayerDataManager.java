bc56363d4de78eeac84071b4d9f15d6ca02e335e
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-09-20 13:39:09 -0700

    Small null type fix

--- src/me/neoblade298/neocore/bukkit/player/PlayerDataManager.java
+++ src/me/neoblade298/neocore/bukkit/player/PlayerDataManager.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 import org.bukkit.Bukkit;
@@ -14,13 +13,13 @@ import org.bukkit.entity.Player;
 import me.neoblade298.neocore.bukkit.io.IOComponent;
 
 public class PlayerDataManager implements IOComponent {
-	private static final Map<String, PlayerTags> tags = Map.of(
-			"neocore", new PlayerTags("neocore", false),
-			"books", new PlayerTags("books", true));
+	private static final PlayerTags neocoreTags = new PlayerTags("neocore", false);
+	private static final PlayerTags bookTags = new PlayerTags("books", true);
+	private static final PlayerTags[] tags = { neocoreTags, bookTags };
 
 	@Override
 	public void savePlayer(Player p, Connection con, List<PreparedStatement> stmts) throws Exception {
-		for (PlayerTags pTags : tags.values()) {
+		for (PlayerTags pTags : tags) {
 			pTags.save(con, stmts, p.getUniqueId());
 		}
 	}
@@ -31,7 +30,7 @@ public class PlayerDataManager implements IOComponent {
 	// Load instead of preload so it has time to save from boss fights
 	@Override
 	public void loadPlayer(Player p, Statement stmt) {
-		for (PlayerTags pTags : tags.values()) {
+		for (PlayerTags pTags : tags) {
 			pTags.load(stmt, p.getUniqueId());
 		}
 	}
@@ -41,13 +40,15 @@ public class PlayerDataManager implements IOComponent {
 	public void cleanup(Connection con, List<PreparedStatement> stmts) throws Exception {
 		for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
 			Player p = Objects.requireNonNull(onlinePlayer);
-			for (PlayerTags pTags : tags.values()) {
+			for (PlayerTags pTags : tags) {
 				pTags.save(con, stmts, p.getUniqueId());
 			}
 		}
 	}
 
 	public static PlayerTags getPlayerTags(String key) {
-		return tags.get(key);
+		if ("neocore".equals(key)) return neocoreTags;
+		if ("books".equals(key)) return bookTags;
+		return null;
 	}
 }
